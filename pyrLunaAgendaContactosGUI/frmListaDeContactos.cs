4cb992db76b105a35b3bce35915978ac602911d9
LunaTomas <Conradoluna20@gmail.com>
2025-09-16 17:31:15 -0300

    agregue el boton eliminar y pude pasar un vector de un frm a otro

--- pyrLunaAgendaContactosGUI/frmListaDeContactos.cs
+++ pyrLunaAgendaContactosGUI/frmListaDeContactos.cs
@@ -12,32 +12,34 @@ namespace pyrLunaAgendaContactosGUI
 {
     public partial class frmListaDeContactos : Form
     {
-        public string[] vecNombres = new string[100];
-        public string[] vecNumeros = new string[100];
+        public string[] vecNombres;
+        public string[] vecNumeros;
         int indice = 0;
         public frmListaDeContactos()
         {
             InitializeComponent();
         }
-
         private void frmListaDeContactos_Load(object sender, EventArgs e)
         {
             lblNombre.Text = vecNombres[0];
             lblNumero.Text = vecNumeros[0];
             btnAnterior.Enabled = false;
+            if (vecNombres.Length == 1 || vecNombres[1] == null)
+            {
+                btnSiguiente.Enabled = false;
+            }
         }
-
         private void btnSiguiente_Click(object sender, EventArgs e)
         {
             //hacer que el boton se desactive cuando llegue a la ultima posicion del vector que no sea null
-            if (vecNombres[indice + 1] != null)
+            if (indice + 1 < vecNombres.Length && vecNombres[indice + 1] != null)
             {
                 indice++;
                 lblNombre.Text = vecNombres[indice];
                 lblNumero.Text = vecNumeros[indice];
                 btnAnterior.Enabled = true;
             }
-            if (vecNombres[indice + 2] == null)
+            if (indice + 1 >= vecNombres.Length || vecNombres[indice + 1] == null)
             {
                 btnSiguiente.Enabled = false;
             }
